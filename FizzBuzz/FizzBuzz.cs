f01d04e943179521180c606abdec9e1b75634165
dhelper <dror.helper@gmail.com>
2015-02-11 14:43:16 +0200

    Initial commit

---
+++ FizzBuzz/FizzBuzz.cs
@@ -0,0 +1,54 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Reactive.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using NUnit.Framework;
+
+namespace FizzBuzz
+{
+    public class FizzBuzz
+    {
+        public static string Generate(int max)
+        {
+            var result = string.Empty;
+
+            if (max > 0)
+            {
+                Observable.Range(1, max)
+                    .Subscribe(i => result += i + ",");
+            }
+
+            return result;
+        }
+    }
+
+    [TestFixture]
+    public class FizzBuzzTests
+    {
+        [Test]
+        public void GivenNumberBelowOne_ReturnEmptyString()
+        {
+            var result = FizzBuzz.Generate(0);
+
+            Assert.That(result, Is.Empty);
+        }
+
+        [Test]
+        public void Given1_Return1()
+        {
+            var result = FizzBuzz.Generate(1);
+
+            Assert.That(result, Is.EqualTo("1,"));
+        }
+
+        [Test]
+        public void Given2_Return12()
+        {
+            var result = FizzBuzz.Generate(2);
+
+            Assert.That(result, Is.EqualTo("1,2,"));
+        }
+    }
+}
