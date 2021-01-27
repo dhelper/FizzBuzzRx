746b8322a4f426615b2db7d8233dd18533fc2fb2
dhelper <dror.helper@gmail.com>
2021-01-27 22:17:37 +0200

    Updated

--- FizzBuzz/FizzBuzz.cs
+++ FizzBuzz/FizzBuzz.cs
@@ -25,8 +25,8 @@ namespace FizzBuzz
                 .Select(_ => "Buzz");
 
             var simpleNumbers = observable
-               .Where(i => i % 3 != 0 && i % 5 != 0)
-               .Select(i => i.ToString());
+                .Where(i => i % 3 != 0 && i % 5 != 0)
+                .Select(i => i.ToString());
 
             var commaDelimiter = observable.Select(_ => ",");
 
@@ -51,26 +51,32 @@ namespace FizzBuzz
             Assert.That(result, Is.Empty);
         }
 
-        [TestCase(1, Result = "1,")]
-        [TestCase(2, Result = "1,2,")]
-        public string GivenNumberUpTo2_ReturnNumbersCommaDelimited(int input)
+        [TestCase(1, "1,")]
+        [TestCase(2, "1,2,")]
+        public void GivenNumberUpTo2_ReturnNumbersCommaDelimited(int input, string expected)
         {
-            return FizzBuzz.Generate(input);
+            var actual = FizzBuzz.Generate(input);
+
+            Assert.That(actual, Is.EqualTo(expected));
         }
 
-        [TestCase(3, Result = "1,2,Fizz,")]
-        [TestCase(6, Result = "1,2,Fizz,4,Buzz,Fizz,")]
-        [TestCase(9, Result = "1,2,Fizz,4,Buzz,Fizz,7,8,Fizz,")]
-        public string GivenNumberDividedBy_ReturnFizzInstead(int input)
+        [TestCase(3, "1,2,Fizz,")]
+        [TestCase(6, "1,2,Fizz,4,Buzz,Fizz,")]
+        [TestCase(9, "1,2,Fizz,4,Buzz,Fizz,7,8,Fizz,")]
+        public void GivenNumberDividedBy_ReturnFizzInstead(int input, string expected)
         {
-            return FizzBuzz.Generate(input);
+            var actual = FizzBuzz.Generate(input);
+
+            Assert.That(actual, Is.EqualTo(expected));
         }
 
-        [TestCase(5, Result = "1,2,Fizz,4,Buzz,")]
-        [TestCase(10, Result = "1,2,Fizz,4,Buzz,Fizz,7,8,Fizz,Buzz,")]
-        public string GivenNumberDividedBy5_ReturnBuzzInstead(int input)
+        [TestCase(5, "1,2,Fizz,4,Buzz,")]
+        [TestCase(10, "1,2,Fizz,4,Buzz,Fizz,7,8,Fizz,Buzz,")]
+        public void GivenNumberDividedBy5_ReturnBuzzInstead(int input, string expected)
         {
-            return FizzBuzz.Generate(input);
+            var actual = FizzBuzz.Generate(input);
+
+            Assert.That(actual, Is.EqualTo(expected));
         }
 
         [Test]
